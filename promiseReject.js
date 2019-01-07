73d4db889809f8d71d024cb4a707d37c7dddde84
nikhil-bacancy <nikhil.patel@bacancytechnology.com>
2019-01-07 11:47:08 +0530

    reject and handle promise

---
+++ promiseReject.js
@@ -0,0 +1,18 @@
+require('es6-promise');
+
+ // 'use strict';
+
+ var promise =  new Promise(function(fulfill, reject){
+ 		setTimeout(function(){
+ 				reject(new Error('REJECTED!'));
+ 		},300);
+ });
+
+
+ function onReject(error){
+ 		console.log(error.message);
+ }
+
+
+
+promise.then(null,onReject);
